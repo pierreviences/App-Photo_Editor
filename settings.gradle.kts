f74fe6318cb4812d396dbd86b89926922ecc85c5
pierreviences <imaniyazid@gmail.com>
2023-08-19 20:15:41 +0700

    feat: Setup Project & Install Plugin

---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+
+        maven { url = uri("https://jitpack.io") }
+        flatDir{
+            dirs("libs")
+        }
+    }
+}
+
+rootProject.name = "Image Editor"
+include(":app")
