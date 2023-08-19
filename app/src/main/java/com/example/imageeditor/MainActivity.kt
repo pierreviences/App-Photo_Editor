54a7c0409b9cd85a668ed1ff399af038f1d6fd25
pierreviences <imaniyazid@gmail.com>
2023-08-19 20:24:31 +0700

    style: Styling Layout Main

--- app/src/main/java/com/example/imageeditor/MainActivity.kt
+++ app/src/main/java/com/example/imageeditor/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
