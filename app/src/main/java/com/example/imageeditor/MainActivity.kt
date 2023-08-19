c937aecab5b21581843436029a929017acc01dea
pierreviences <imaniyazid@gmail.com>
2023-08-19 20:28:01 +0700

    feat: Added Image Selection and Navigation to EditActivity

--- app/src/main/java/com/example/imageeditor/MainActivity.kt
+++ app/src/main/java/com/example/imageeditor/MainActivity.kt
@@ -1,13 +1,42 @@
 package com.example.imageeditor
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.imageeditor.databinding.ActivityMainBinding
+import com.github.dhaval2404.imagepicker.ImagePicker
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var 
+    private lateinit var binding: ActivityMainBinding
+    private var imageUri: Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.selectImageCard.setOnClickListener {
+            ImagePicker.with(this)
+                .crop()
+                .start()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        try{
+            if(data!= null){
+                imageUri = data.data
+                val intent = Intent(this, EditActivity::class.java)
+                intent.putExtra("imageUri", imageUri.toString())
+                startActivity(intent)
+            }
+        }catch (e:Exception){
+
+        }
     }
 }
